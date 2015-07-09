d08d7d6c5ecc0d2cd5552e8bed1a9cfba5f2c3ba
eblondel <emmanuel.blondel1@gmail.com>
2015-07-09 19:59:15 +0200

    #29 add basic collection-specific codelist parser for FAO areas

---
+++ collection/src/main/java/org/fao/fi/gems/collection/parsers/FsaCodelistParser.java
@@ -0,0 +1,108 @@
+package org.fao.fi.gems.collection.parsers;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.fao.fi.gems.codelist.CodelistParser;
+import org.fao.fi.gems.collection.properties.EezProperty;
+import org.fao.fi.gems.collection.properties.FsaProperty;
+import org.fao.fi.gems.entity.EntityCode;
+import org.fao.fi.gems.entity.GeographicEntity;
+import org.fao.fi.gems.entity.GeographicEntityImpl;
+import org.fao.fi.gems.lod.entity.common.FLODEezEntity;
+import org.fao.fi.gems.metaobject.GeographicMetaObjectProperty;
+import org.fao.fi.gems.model.GemsConfig;
+import org.fao.fi.gems.model.settings.data.filter.DataObjectFilter;
+import org.fao.fi.gems.util.Utils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+/**
+ * A codelist parser for FAO Areas
+ * 
+ * @author eblondel <emmanuel.blondel@fao.org>
+ *
+ */
+public class FsaCodelistParser implements CodelistParser {
+	
+	@Override
+	public Set<GeographicEntity> getCodelist(GemsConfig config) {
+		
+		String owner = Utils.whoIsOwner(config);
+		
+		Set<GeographicEntity> fsaCodelist = new HashSet<GeographicEntity>();
+		
+		JsonReader reader = null;
+		try {
+			// read Geoserver data as json
+			String url = config.getSettings().getPublicationSettings().getCodelistURL();
+			URL dataURL = new URL(url);
+		
+			reader = new JsonReader(new InputStreamReader(dataURL.openStream()));
+			JsonParser parser = new JsonParser();
+			JsonObject flodJsonObject = parser.parse(reader).getAsJsonObject();
+
+			JsonArray bindings = flodJsonObject.get("features").getAsJsonArray();
+
+			if (bindings.size() > 0) {
+				for(int i = 0;i<bindings.size();i++){
+					JsonObject obj = bindings.get(i).getAsJsonObject().get("properties").getAsJsonObject();
+					String fsa = obj.get("F_CODE").getAsString();
+					DataObjectFilter fsaFilter = config.getSettings().getGeographicServerSettings().getFilters().getData().get(0);
+					EntityCode fsaCode = new EntityCode(fsaFilter, fsa);
+					List<EntityCode> fsaCodeStack = Arrays.asList(fsaCode);
+
+					GeographicEntity entity = null;
+					
+					//wrapEntity by default is true
+					//if there is a list of subset then wrap entity only for those ones
+					boolean wrapEntity = Utils.wrapEntity(config, fsa);
+					
+					if(wrapEntity){
+						try {
+							String collection = config.getSettings().getPublicationSettings().getCollectionType();
+							
+							Map<GeographicMetaObjectProperty, List<String>> properties = new HashMap<GeographicMetaObjectProperty, List<String>>();
+							properties.put(FsaProperty.FAO, Arrays.asList(Utils.buildMetadataIdentifier(owner, collection, fsa)));
+							properties.put(FsaProperty.CWP, Arrays.asList(fsa));
+							
+							
+							FLODEezEntity flodEntity = new FLODEezEntity(fsa);
+							if(flodEntity.content() != null){
+								properties.put(EezProperty.FLOD, Arrays.asList(flodEntity.authorityUri()));
+							}
+							
+							entity = new GeographicEntityImpl(owner, collection, fsaCodeStack, fsa, properties);
+							fsaCodelist.add(entity);
+						} catch (URISyntaxException e) {
+							e.printStackTrace();
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					
+				}
+			}
+
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return fsaCodelist;
+	}
+
+}
---
+++ collection/src/main/java/org/fao/fi/gems/collection/properties/FsaProperty.java
@@ -0,0 +1,47 @@
+package org.fao.fi.gems.collection.properties;
+
+import org.fao.fi.gems.entity.EntityAuthority;
+import org.fao.fi.gems.metaobject.GeographicMetaObjectProperty;
+
+/**
+ * FsaProperty enumeration
+ * (to use in Fsa codelists)
+ * 
+ * @author eblondel
+ *
+ */
+public enum FsaProperty implements GeographicMetaObjectProperty{
+		
+	FAO (EntityAuthority.FAO, true, true, true),
+	FLOD (EntityAuthority.FLOD, true, true, true),
+	CWP (EntityAuthority.CWP, true, true, false);
+	
+	private final Object object;
+	private final boolean isAuthority;
+	private final boolean isThesaurus;
+	private final boolean containsURIs;
+	
+	FsaProperty(Object object, boolean isAuthority, boolean isThesaurus, boolean containsURIs){
+		this.object = object;
+		this.isAuthority = isAuthority;
+		this.isThesaurus = isThesaurus;
+		this.containsURIs = containsURIs;
+	}
+	
+	public Object getObject(){
+		return this.object;
+	}
+
+	public boolean isAuthority(){
+		return this.isAuthority;
+	}
+	
+	public boolean isThesaurus() {
+		return this.isThesaurus;
+	}
+
+	public boolean containsURIs() {
+		return this.containsURIs;
+	}
+	
+}
