a756e40ea0db8e5294cfb9d0d3d7ad9de61140a0
eblondel <emmanuel.blondel1@gmail.com>
2015-09-16 09:33:35 +0200

    sleep

--- publisher/src/main/java/org/fao/fi/gems/publisher/MetadataPublisher.java
+++ publisher/src/main/java/org/fao/fi/gems/publisher/MetadataPublisher.java
@@ -188,7 +188,7 @@ public class MetadataPublisher {
 			} catch (Exception e) {
 				throw new PublicationException("Fail to update metadata", e);
 			}finally{
-				long sleep = 2;
+				long sleep = 5;
 				Thread.sleep(sleep*1000);
 				LOGGER.info("Sleeping "+sleep+" seconds.");
 			}
